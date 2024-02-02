9ad3a0c43ff2648afbbe541e98b3ab7a744e4623
Carlos De la Cruz <me@heyimcarlos.dev>
2024-02-01 19:36:28 -0500

    chore: finish day 1 part 2

--- 2023/rust/day_01/src/bin/p2.rs
+++ 2023/rust/day_01/src/bin/p2.rs
@@ -1,7 +1,75 @@
-mod util;
+use day_01::{util, INPUT_ENDPOINT};
 
 fn main() {
-    println!("day 1 part 2");
-    // common::
-    // util::fetch();
+    let input_exists = std::path::Path::new("input.txt").exists();
+    if !input_exists {
+        let contents = util::fetch(INPUT_ENDPOINT).unwrap();
+        let _ = std::fs::write("input.txt", contents);
+    }
+    let contents = std::fs::read_to_string("input.txt").unwrap();
+    println!("p2 result: {}", p2(contents));
+}
+
+fn p2(contents: String) -> String {
+    let output = contents
+        .lines()
+        .map(|line| {
+            let mut result = vec![];
+            let mut index = 0;
+            while index < line.len() {
+                let curr_char = line.chars().nth(index).unwrap();
+                let reduced_line = &line[index..];
+
+                if reduced_line.starts_with("one") {
+                    result.push(1);
+                } else if reduced_line.starts_with("two") {
+                    result.push(2);
+                } else if reduced_line.starts_with("three") {
+                    result.push(3);
+                } else if reduced_line.starts_with("four") {
+                    result.push(4);
+                } else if reduced_line.starts_with("five") {
+                    result.push(5);
+                } else if reduced_line.starts_with("six") {
+                    result.push(6);
+                } else if reduced_line.starts_with("seven") {
+                    result.push(7);
+                } else if reduced_line.starts_with("eight") {
+                    result.push(8);
+                } else if reduced_line.starts_with("nine") {
+                    result.push(9);
+                } else if curr_char.is_digit(10) {
+                    let val = curr_char.to_digit(10).unwrap() as i32;
+                    result.push(val);
+                }
+                index += 1;
+            }
+            let first = result.first().expect("should be a number");
+            let last = result.last().expect("should be a number");
+            let total = format!("{first}{last}")
+                .parse::<i32>()
+                .expect("should be a valid number");
+            total
+        })
+        .sum::<i32>();
+
+    output.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test] // turns function into a test;
+    fn test_p2() {
+        let result = p2("two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"
+            .to_string());
+        assert_eq!(result, "281".to_string());
+    }
 }
